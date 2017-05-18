e83dcf123f85401bfd9e34476470a0be39fd3ae8
Martin Booth <mbooth@seek.com.au>
2017-05-18 11:35:36 +1000

    Put in some navigation

--- scripts.js
+++ scripts.js
@@ -6,12 +6,25 @@ requirejs.config({
 });
 
 require(['knockout'], function (ko) {
+	class NavigationViewModel {
+		constructor() {
+			this.currentPage = ko.observable("my-data");
+		}
+
+		navigate(page) {
+			this.currentPage(page);
+		}
+	}
 
 	ko.components.register('my-data', {
 		viewModel: { require: 'pages/mydata' },
 		template: { require: 'text!pages/mydata.html' }
 	});
-	
-	ko.applyBindings();
-	
+
+	ko.components.register('competitors', {
+		viewModel: { require: 'pages/competitors/competitors' },
+		template: { require: 'text!pages/competitors/competitors.html' }
+	});
+
+	ko.applyBindings(new NavigationViewModel());
 });
