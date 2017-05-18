d8236a7ace072523bd097c781bd6080df2d0f0df
rmlalford <r.m.l.alford@gmail.com>
2017-05-18 11:14:40 +1000

    Put data in json and styles

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 (async function(){
 	async function getData()
 	{
-		var response = await fetch('api/getdata.php');
+		var response = await fetch('data/data.json');
 
 		return await response.json();
 	}
@@ -11,30 +11,6 @@
 	var data = await getData();
 
     ko.applyBindings(data);
-	//$('.data').text(data);
-
-
-
-
-	// // On page load, set inital page to home.html
-	// $.get("pages/mydata.html", function(data){
- //    	$('.content').replaceWith(data);
-	// });
-
-	// // Change page, basedon menu item click
-	// $("#mydata").click(function(){
- //    	$.get("pages/mydata.html", function(data){
- //    		$('.content').replaceWith(data);
-	// 	});
-	// 	getData();
-	// });
-
-	// $("#competitors").click(function(){
- //    	$.get("pages/competitors.html", function(data){
- //    		$('.content').replaceWith(data);
-	// 	});
-	// 	getData();
-	// });	
 
 })();
 
