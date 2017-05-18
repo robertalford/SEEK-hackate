e29123c28e787e1e33a67c3710aa92216626487a
Martin Booth <mbooth@seek.com.au>
2017-05-18 11:12:35 +1000

    Move to own component

---
+++ pages/mydata.js
@@ -0,0 +1,7 @@
+define(['knockout'], function(ko) {
+    return function ViewModel(params) {
+        this.mydata= [
+          {CompanyName: "telstra"}
+        ]
+    }
+});
--- scripts.js
+++ scripts.js
@@ -1,41 +1,60 @@
-
-(async function(){
-	async function getData()
-	{
-		var response = await fetch('api/getdata.php');
-
-		return await response.json();
+requirejs.config({
+	paths: {
+		text: './node_modules/text/text',
+		knockout: './node_modules/knockout/build/output/knockout-latest'
 	}
+});
 
-	// On page load, get initial data
-	var data = await getData();
+require(['knockout'], function (ko) {
 
-    ko.applyBindings(data);
-	//$('.data').text(data);
+	ko.components.register('my-data', {
+		viewModel: { require: 'pages/mydata' },
+		template: { require: 'text!pages/mydata.html' }
+	});
+	
+	ko.applyBindings();
 
+	// (async function () {
+	// 	async function getData() {
+	// 		return {
+	// 			mydata: [
+	// 				{ CompanyName: "Telstra" }
+	// 			]
+	// 		};
+	// 		// var response = await fetch('api/getdata.php');
 
+	// 		// return await response.json();
+	// 	}
 
+	// 	// On page load, get initial data
+	// 	var data = await getData();
 
-	// // On page load, set inital page to home.html
-	// $.get("pages/mydata.html", function(data){
- //    	$('.content').replaceWith(data);
-	// });
-
-	// // Change page, basedon menu item click
-	// $("#mydata").click(function(){
- //    	$.get("pages/mydata.html", function(data){
- //    		$('.content').replaceWith(data);
-	// 	});
-	// 	getData();
-	// });
-
-	// $("#competitors").click(function(){
- //    	$.get("pages/competitors.html", function(data){
- //    		$('.content').replaceWith(data);
-	// 	});
-	// 	getData();
-	// });	
-
-})();
+	// 	//$('.data').text(data);
 
 
+
+
+	// 	// // On page load, set inital page to home.html
+	// 	// $.get("pages/mydata.html", function(data){
+	// 	//    	$('.content').replaceWith(data);
+	// 	// });
+
+	// 	// // Change page, basedon menu item click
+	// 	// $("#mydata").click(function(){
+	// 	//    	$.get("pages/mydata.html", function(data){
+	// 	//    		$('.content').replaceWith(data);
+	// 	// 	});
+	// 	// 	getData();
+	// 	// });
+
+	// 	// $("#competitors").click(function(){
+	// 	//    	$.get("pages/competitors.html", function(data){
+	// 	//    		$('.content').replaceWith(data);
+	// 	// 	});
+	// 	// 	getData();
+	// 	// });	
+
+	// })();
+
+
+});
