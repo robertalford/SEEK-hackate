f998ed11104454ca7edb5ee991d2dd5e1358384e
rmlalford <r.m.l.alford@gmail.com>
2017-05-18 10:29:15 +1000

    Added knockout

--- scripts.js
+++ scripts.js
@@ -1,42 +1,41 @@
 
-$(document).ready(function(){
-
-	function getData()
+(async function(){
+	async function getData()
 	{
-	     var result = null;
-	     var scriptUrl = "api/getdata.php"
-	     $.ajax({
-	        url: scriptUrl,
-	        type: 'get',
-	        success: function(data) {
-	            $('.data').text(data);
-	        } 
-	     });
+		var response = await fetch('api/getdata.php');
+
+		return await response.json();
 	}
 
 	// On page load, get initial data
-	getData();
+	var data = await getData();
 
-	// On page load, set inital page to home.html
-	$.get("pages/mydata.html", function(data){
-    	$('.content').replaceWith(data);
-	});
+    ko.applyBindings(data);
+	//$('.data').text(data);
 
-	// Change page, basedon menu item click
-	$("#mydata").click(function(){
-    	$.get("pages/mydata.html", function(data){
-    		$('.content').replaceWith(data);
-		});
-		getData();
-	});
 
-	$("#competitors").click(function(){
-    	$.get("pages/competitors.html", function(data){
-    		$('.content').replaceWith(data);
-		});
-		getData();
-	});	
 
-});
+
+	// // On page load, set inital page to home.html
+	// $.get("pages/mydata.html", function(data){
+ //    	$('.content').replaceWith(data);
+	// });
+
+	// // Change page, basedon menu item click
+	// $("#mydata").click(function(){
+ //    	$.get("pages/mydata.html", function(data){
+ //    		$('.content').replaceWith(data);
+	// 	});
+	// 	getData();
+	// });
+
+	// $("#competitors").click(function(){
+ //    	$.get("pages/competitors.html", function(data){
+ //    		$('.content').replaceWith(data);
+	// 	});
+	// 	getData();
+	// });	
+
+})();
 
 
